b729965e3a0dd86212705847620c868485b80219
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-02-23 13:00:44 +0100

    move file to test folder

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -13,7 +13,7 @@ import visualsvm.*;
 public class Test {
     public static void main(String[] args) throws Exception {
    			
-    	String fileName = "quicksort.fool";
+    	String fileName = "src/test/quicksort.fool";
 
     	CharStream chars = CharStreams.fromFileName(fileName);
     	FOOLLexer lexer = new FOOLLexer(chars);
